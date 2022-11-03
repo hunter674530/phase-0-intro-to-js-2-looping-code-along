8e35652dbd2d06966000f1b2ee2ecac3c502edc3
Hunter Wood <hunter674530@gmail.com>
2022-11-03 11:58:35 -0600

    commit tree ced41adbabeb3764d462fcf402731073b3f1704f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+
+const writtenCards = [];
+function writeCards(names, event){
+  for(let counter = 1; counter <= names.length -1; counter+=1){
+    writtenCards.push(`Thank you, ${names[counter]}, for the wonderful ${event} gift!`);
+    
+    
+  }
+  return writtenCards;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(num){
+  for(num; num>= 0; num--){
+    console.log(num);
+  }
+}
+countDown(11);
